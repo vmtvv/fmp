1e2d385b6b86a4ae3f123da560015eab117b905d
Vladimir Matveev <v.a.matveev96@gmail.com>
2020-05-03 14:46:49 +0300

    Add custom route animation to details page.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:fmp/app_routes.dart';
 import 'package:fmp/details_page.dart';
 import 'package:fmp/home_page.dart';
+import 'package:fmp/rotation_route.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,9 +13,15 @@ class MyApp extends StatelessWidget {
     return CupertinoApp(
       title: 'Flutter Demo',
       initialRoute: AppRoutes.home,
-      routes: {
-        AppRoutes.home: (context) => HomePage(title: 'Rainbow Maker'),
-        AppRoutes.details: (context) => DetailsPage(title: 'Details')
+      onGenerateRoute: (settings) {
+        switch (settings.name){
+          case AppRoutes.home:
+            return CupertinoPageRoute(builder: (context) => HomePage(title: "Rainbow Maker"));
+            break;
+          case AppRoutes.details:
+            return RotationRoute(page: DetailsPage(title: "Details"));
+            break;
+        }
       },
     );
   }
